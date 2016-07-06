518a77da0dd42e500d0880294ce996a35f682ba7
ohsnapitsjojo <j.truongle@hotmail.com>
2016-07-06 18:04:39 +0200

    nummerierung auf dem plot fertig

--- HA4/plotPoints.m
+++ HA4/plotPoints.m
@@ -8,5 +8,11 @@ function [ output_args ] = plotPoints( P1 )
     zlabel('z');
     set(gca,'ZDir','reverse');
     set(gca,'YDir','reverse');
+    nKP= size(P1,1);
+    name = linspace(1,nKP,nKP);
+    nameCell = mat2cell(name,1,ones(1,nKP));
+    
+    text(P1(:,3), P1(:,1), P1(:,2), nameCell)
+
 end
 
